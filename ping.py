958be4a3ace2b730ffebee077cead1fa37085572
Ukrainian-tanker228 <143322234+Ukrainian-tanker228@users.noreply.github.com>
2023-09-03 13:38:41 +0300

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,13 @@
+from pygame import*
+bg = (240, 0, 250)
+wind_width = 500
+wind_height = 500
+window = display.set_mode((wind_width, wind_height))
+window.fill(back)
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, player_width, player_height):
+        super().__init__()
+        self.image = image.load(loadimage)
+        self.speed = player_speed
+        self.rect.x=player_x
+        self.rect.y=player_y
